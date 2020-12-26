2df4ae71e36c75d1a86d1c2a1fa696fc437b98f8
thayseingrid <thayse.sdh@gmail.com>
2020-12-26 10:57:11 -0300

    Add files via upload

---
+++ negativo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n1, n2, n3, n4, n5;
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+    cin >> n4;
+    cin >> n5;
+    
+    if (n1 < 0) {
+        cout << n1 << " ";
+    } 
+
+    if (n2 < 0) {
+        cout << n2 << " ";
+    }
+    
+    if (n3 < 0) { 
+        cout << n3 << " ";
+    } 
+
+    if (n4 < 0) {
+        cout << n4 << " ";
+    } 
+
+    if (n5 < 0) {
+        cout << n5 << " ";
+    }
+    cout << endl;
+}
